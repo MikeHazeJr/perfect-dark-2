8aa1463577b245d9b3fbb5914c0b614ae6b4d880
MikeHazeJr <mikehaysjr@aol.com>
2026-03-21 18:52:53 -0400

    Build v0.0.1 - auto-commit before build

--- src/game/challengeinit.c
+++ src/game/challengeinit.c
@@ -5,12 +5,15 @@
 #include "bss.h"
 #include "data.h"
 #include "types.h"
+#include "system.h"
 
 void challengesInit(void)
 {
 	struct mpconfigfull *mpconfig;
 	u8 buffer[0x1ca];
 	s32 i;
+
+	sysLogPrintf(LOG_NOTE, "CHALLENGES: starting init, %d challenges", (s32)ARRAYCOUNT(g_MpChallenges));
 
 	for (i = 0; i < ARRAYCOUNT(g_MpChallenges); i++) {
 		g_MpChallenges[i].availability = 0;
@@ -19,14 +22,23 @@ void challengesInit(void)
 		g_MpChallenges[i].completions[2] = 0;
 		g_MpChallenges[i].completions[3] = 0;
 
+		sysLogPrintf(LOG_NOTE, "CHALLENGES: [%d] loading confignum=%d", i, g_MpChallenges[i].confignum);
 		mpconfig = challengeLoad(i, buffer, 0x1ca);
+		sysLogPrintf(LOG_NOTE, "CHALLENGES: [%d] loaded, mpconfig=%p", i, (void *)mpconfig);
 		challengeForceUnlockConfigFeatures(&mpconfig->config, g_MpChallenges[i].unlockfeatures, 16, i);
+		sysLogPrintf(LOG_NOTE, "CHALLENGES: [%d] unlockFeatures done", i);
 	}
 
+	sysLogPrintf(LOG_NOTE, "CHALLENGES: challenges loop done, starting presets");
+
 	for (i = 0; i < mpGetNumPresets(); i++) {
+		sysLogPrintf(LOG_NOTE, "CHALLENGES: preset[%d] confignum=%d", i, g_MpPresets[i].confignum);
 		mpconfig = challengeLoadConfig(g_MpPresets[i].confignum, buffer, 0x1ca);
+		sysLogPrintf(LOG_NOTE, "CHALLENGES: preset[%d] loaded", i);
 		challengeForceUnlockConfigFeatures(&mpconfig->config, g_MpPresets[i].requirefeatures, 16, -1);
 	}
 
+	sysLogPrintf(LOG_NOTE, "CHALLENGES: presets done, calling determinUnlockedFeatures");
 	challengeDetermineUnlockedFeatures();
+	sysLogPrintf(LOG_NOTE, "CHALLENGES: init complete");
 }
