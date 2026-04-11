93208ec3dd3a479027e34584bcee90daf74ccce5
Mike Hays <mikehaysjr@aol.com>
2026-04-11 16:50:24 -0400

    Build v0.0.77 - auto-commit before build

---
+++ .claude/b8-dev-verify/CMakeFiles/pd.dir/port/fast3d/pdgui_audio.cpp.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/pd.dir/port/fast3d/pdgui_audio.cpp.obj: \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/fast3d/pdgui_audio.cpp \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/.claude/b8-dev-verify/port/include/versioninfo.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/pdgui_audio.h
---
+++ .claude/b8-dev-verify/CMakeFiles/pd.dir/port/fast3d/pdgui_nineslice.cpp.obj.d
@@ -0,0 +1,50 @@
+CMakeFiles/pd.dir/port/fast3d/pdgui_nineslice.cpp.obj: \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/fast3d/pdgui_nineslice.cpp \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/.claude/b8-dev-verify/port/include/versioninfo.h \
+ C:/msys64/mingw64/include/stdio.h \
+ C:/msys64/mingw64/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/_mingw_off_t.h \
+ C:/msys64/mingw64/include/swprintf.inl \
+ C:/msys64/mingw64/include/sec_api/stdio_s.h \
+ C:/msys64/mingw64/include/c++/15.2.0/stdlib.h \
+ C:/msys64/mingw64/include/c++/15.2.0/cstdlib \
+ C:/msys64/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/c++config.h \
+ C:/msys64/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/os_defines.h \
+ C:/msys64/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/cpu_defines.h \
+ C:/msys64/mingw64/include/c++/15.2.0/pstl/pstl_config.h \
+ C:/msys64/mingw64/include/stdlib.h \
+ C:/msys64/mingw64/include/corecrt_wstdlib.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/limits.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/syslimits.h \
+ C:/msys64/mingw64/include/limits.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/sec_api/stdlib_s.h \
+ C:/msys64/mingw64/include/malloc.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/mm_malloc.h \
+ C:/msys64/mingw64/include/errno.h \
+ C:/msys64/mingw64/include/c++/15.2.0/bits/std_abs.h \
+ C:/msys64/mingw64/include/string.h \
+ C:/msys64/mingw64/include/sec_api/string_s.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h C:/msys64/mingw64/include/ctype.h \
+ C:/msys64/mingw64/include/corecrt_wctype.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/include/PR/ultratypes.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/fast3d/imgui/imgui.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/fast3d/imgui/imconfig.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/float.h \
+ C:/msys64/mingw64/include/float.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdarg.h \
+ C:/msys64/mingw64/include/stdarg.h \
+ C:/msys64/mingw64/include/_mingw_stdarg.h \
+ C:/msys64/mingw64/include/assert.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/pdgui_nineslice.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/pdgui_theme.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/assetcatalog.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/fs.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/system.h \
+ C:/Users/mikeh/Perfect-Dark-2/perfect_dark-mike/port/include/fs.h
